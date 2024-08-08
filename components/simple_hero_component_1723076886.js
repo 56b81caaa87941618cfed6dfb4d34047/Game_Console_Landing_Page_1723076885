be878af4585ae9c16cd43962dec515346745e2e0
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-08 04:21:22 +0000

    Code generation

--- components/simple_hero_component_1723076886.js
+++ components/simple_hero_component_1723076886.js
@@ -9,7 +9,7 @@ Vue.component("simple_hero_component_1723076886", {
                 <div id="hero-text-container" class="mr-auto place-self-center lg:col-span-7">
                     
                     <div id="hero-title-container-text" class="flex">
-                        <h1 id="hero-title" class="flex-1 max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl text-gray-900 dark:text-white">Elevate Your Gaming Experience</h1>
+                        <h1 id="hero-title" class="flex-1 max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl text-red-600 dark:text-red-500">Elevate Your Gaming Experience</h1>
                     </div>
 
                     <div id="hero-subtitle-container" class="flex">
